c8be7cb38b1e9e3a70aefaee7d4e27ccd9406ccf
keizah7 <murashka.arturas@gmail.com>
2020-03-28 19:57:23 +0200

    Touch It

--- app/Task.php
+++ app/Task.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $guarded = [];
+    protected $touches = ['project'];
 
     public function project()
     {
--- app/User.php
+++ app/User.php
@@ -39,6 +39,6 @@ class User extends Authenticatable
 
     public function projects()
     {
-        return $this->hasMany(Project::class);
+        return $this->hasMany(Project::class)->latest('updated_at');
     }
 }
